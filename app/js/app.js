932f49511a939f68a5a026aede2969fac6639c61
roman01la <roman01la@romanliutikov.com>
2014-01-08 18:12:32 +0200

    adjust orbit slider, pages content & fill About page with info and screenshots

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,11 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+$(document).foundation({
+  orbit: {
+    timer_speed: 5000,
+    resume_on_mouseout: true,
+    slide_number: false,
+    timer: false,
+    bullets: false
+  }
+});
